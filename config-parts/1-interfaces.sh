a858b4ade6e71ced62d167834651e4173a5b32d8
trosvald <eko.pur25@gmail.com>
2024-02-18 21:34:02 +0700

    chore: reconfigure

--- config-parts/1-interfaces.sh
+++ config-parts/1-interfaces.sh
@@ -20,7 +20,7 @@ set interfaces bonding bond0 vif 1614 address '172.16.14.254/24'
 set interfaces bonding bond0 vif 1614 description 'CCTV'
 
 set interfaces pppoe pppoe0 authentication password "${SECRET_ISP_AUTH_PASSWORD}"
-set interfaces pppoe pppoe0 authentication username "${SECRET_ISP_AUTH_USER}
+set interfaces pppoe pppoe0 authentication username "${SECRET_ISP_AUTH_USER}"
 set interfaces pppoe pppoe0 description 'WAN - PPPoE'
 set interfaces pppoe pppoe0 ip adjust-mss 'clamp-mss-to-pmtu'
 set interfaces pppoe pppoe0 mtu '1492'
@@ -29,6 +29,6 @@ set interfaces pppoe pppoe0 source-interface 'eth0'
 set interfaces wireguard wg01 address '10.12.12.1/24'
 set interfaces wireguard wg01 description 'Wireguard'
 set interfaces wireguard wg01 peer monosense allowed-ips '10.12.12.2/32'
-set interfaces wireguard wg01 peer monosense public-key "${SECRET_WIREGUARD_PUBLIC_KEY}
+set interfaces wireguard wg01 peer monosense public-key "${SECRET_WIREGUARD_PUBLIC_KEY}"
 set interfaces wireguard wg01 port '51820'
 set interfaces wireguard wg01 private-key "${SECRET_WIREGUARD_PRIVATE_KEY}
